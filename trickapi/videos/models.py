119e0a8a37d318abbe24202e3e575eeae8a53546
Tuss4 <tuss4dbfn@gmail.com>
2015-09-01 07:46:48 -0500

    Starting to setup models.

--- trickapi/videos/models.py
+++ trickapi/videos/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+YT = 0
+IG = 1
+VN = 2
+
+VIDEO_TYPES = (
+    (YT, "YouTube"),
+    (IG, "Instagram"),
+    (VN, "Vine"),
+)
+
+
+class Video(models.Model):
+
+    video_type = models.CharField()
