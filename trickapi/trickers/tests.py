2fa990562ac37502b6bad7a2d6dd51ac7baa03af
Tuss4 <tuss4dbfn@gmail.com>
2015-09-04 13:12:18 -0500

    Finished tricker creation tests.

--- trickapi/trickers/tests.py
+++ trickapi/trickers/tests.py
@@ -1,3 +1,36 @@
-from django.test import TestCase
+from django.core.urlresolvers import reverse
 
-# Create your tests here.
+from rest_framework.test import APITestCase
+from rest_framework import status
+
+
+class TrickerTests(APITestCase):
+
+    create_url = reverse("register_tricker")
+
+    def test_create_tricker(self):
+        response = self.client.post(self.create_url, {
+            "email": "youngtricklord@trickhouse.io",
+            "password": "butterflytwistround"
+        })
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_create_tricker_bad_request(self):
+        response = self.client.post(self.create_url, {
+            "email": "youngtricklord",
+            "password": "butterflytwistround"
+        })
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_tricker_conflict(self):
+        response = self.client.post(self.create_url, {
+            "email": "youngtricklord@trickhouse.io",
+            "password": "butterflytwistround"
+        })
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        response = self.client.post(self.create_url, {
+            "email": "youngtricklord@trickhouse.io",
+            "password": "butterflytwistround"
+        })
+        self.assertEqual(response.status_code, status.HTTP_409_CONFLICT)
