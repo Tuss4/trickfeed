6df7b55b930981dd564e9d26ae24c0093586f4ec
Tuss4 <tuss4dbfn@gmail.com>
2015-09-13 01:56:52 -0500

    Test get index name.

--- trickapi/eswrapper/tests.py
+++ trickapi/eswrapper/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from .mixins import get_prefix
+from videos.models import Video
 
 
 class ESWrapperTests(TestCase):
@@ -7,3 +8,7 @@ class ESWrapperTests(TestCase):
     def test_get_prefix(self):
         prefix = get_prefix()
         self.assertTrue(prefix.startswith('test_'))
+
+    def test_get_index_name(self):
+        self.assertEqual(
+            Video.get_index_name(), 'test_trickapi_video_index')
