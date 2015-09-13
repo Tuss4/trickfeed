819f7d9c0d35b01a67f0eb18c1ac4f0f528fada4
Tuss4 <tuss4dbfn@gmail.com>
2015-09-13 02:26:55 -0500

    Some documentation.

--- trickapi/eswrapper/mixins.py
+++ trickapi/eswrapper/mixins.py
@@ -10,6 +10,14 @@ def get_prefix():
 
 class ESWrapperMixin(object):
 
+    '''ESWrapper Mixin for django models'''
+
     @classmethod
     def get_index_name(cls):
+        '''Get the elasticsearch index name.'''
         return "{0}{1}_index".format(get_prefix(), cls.__name__.lower())
+
+
+class ESTestMixin(object):
+
+    pass
