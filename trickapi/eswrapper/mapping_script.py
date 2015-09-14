f633054292c67212ba67f975b6bccb4b933a0ac6
Tuss4 <tuss4dbfn@gmail.com>
2015-09-14 10:55:42 -0500

    Clean stuff up.

--- trickapi/eswrapper/mapping_script.py
+++ trickapi/eswrapper/mapping_script.py
@@ -9,13 +9,15 @@ def update_properties_dict(props, k, field_type):
             props[k] = {"type": "long"}
         if field_type == CHARFIELD:
             props[k] = {"type": "string"}
+        if field_type == DATEFIELD:
+            props[k] = {"type": "string"}
 
 
 def create_mapping(app_config, model):
     m = app_config.get_model(model)
     fields = m._meta.get_fields()
     mapping_name = '{}_MAPPING'.format(m.__name__.upper())
-    print mapping_name
+
     map_dict = {
         "mappings": {
             m.get_document_type(): {
@@ -23,12 +25,11 @@ def create_mapping(app_config, model):
             }
         }
     }
-    print map_dict
+
     for field in fields:
         props = map_dict['mappings'][m.get_document_type()]['properties']
         update_properties_dict(props, field.name, field.get_internal_type())
 
-    print map_dict
     path = app_config.path + '/es_mappings.py'
     f = open(path, 'w')
     f.write('{0} = {1}'.format(mapping_name, map_dict))
--- trickapi/videos/es_mappings.py
+++ trickapi/videos/es_mappings.py
@@ -1,1 +1,1 @@
-VIDEO_MAPPING = {'mappings': {'video_document': {'properties': {'video_id': {'type': 'string'}, 'thumbnail_url': {'type': 'string'}, u'id': {'type': 'long'}, 'video_type': {'type': 'string'}, 'title': {'type': 'string'}}}}}
+VIDEO_MAPPING = {'mappings': {'video_document': {'properties': {'title': {'type': 'string'}, 'video_id': {'type': 'string'}, 'video_type': {'type': 'string'}, 'thumbnail_url': {'type': 'string'}, 'date_added': {'type': 'string'}, 'id': {'type': 'long'}}}}}
