b4bfae12ec48519cc4f60d33f402725a5bbfd89f
Tuss4 <tuss4dbfn@gmail.com>
2015-09-16 12:42:15 -0500

    Write to stderr.

--- trickapi/eswrapper/management/commands/eswrap.py
+++ trickapi/eswrapper/management/commands/eswrap.py
@@ -9,18 +9,21 @@ class Command(AppCommand):
     def add_arguments(self, parser):
         super(Command, self).add_arguments(parser)
         parser.add_argument(
-            '--new-index', dest='new-index',  default=False, type=bool,
+            '--new-index', dest='new_index',  default=False, type=bool,
             help='Flag to indicate that a new index is being created.')
         parser.add_argument(
-            '--model', dest='model-name',
+            '--model', dest='model_name',
             help='Model name for index creation.')
+        parser.add_argument(
+            '--create-documents', dest='create_docs')
 
     def handle_app_config(self, app_config, **options):
-        n_index = options.get('new-index')
-        m_name = options.get('model-name')
+        n_index = options.get('new_index')
+        m_name = options.get('model_name')
         if n_index is True:
             if not m_name:
-                self.stdout.write('No model name specified.')
-            self.stdout.write('Creating index.')
-            create_index(app_config, m_name)
-            self.stdout.write('Done.')
+                self.stderr.write('No model name specified.')
+            else:
+                self.stdout.write('Creating index.')
+                create_index(app_config, m_name)
+                self.stdout.write('Done.')
