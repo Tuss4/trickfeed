44af9c4e1f56792f665024abc9422bde44323409
Tuss4 <tuss4dbfn@gmail.com>
2015-09-22 14:04:44 -0500

    Indentation issues.

--- trickapi/eswrapper/managers.py
+++ trickapi/eswrapper/managers.py
@@ -7,18 +7,17 @@ class ESManager(object):
 
     def __init__(self, model=None):
         if model:
-        self.model = model
+            self.model = model
 
     def get_model(self):
         return self.model
 
     def all(self):
         return ES.search(
-        index=[self.get_model().get_index_name()],
-        doc_type=[self.get_model().get_document_type()],
-        q='*',
-        size=self.get_model().objects.count()
-        )['hits']['hits']
+            index=[self.get_model().get_index_name()],
+            doc_type=[self.get_model().get_document_type()],
+            q='*',
+            size=self.get_model().objects.count())['hits']['hits']
 
     # TODO: Finish this function
     def search(self, body):
