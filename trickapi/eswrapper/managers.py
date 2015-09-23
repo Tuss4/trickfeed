907702620c1100bc1ecf0c10fb001124f3c6c385
Tuss4 <tuss4dbfn@gmail.com>
2015-09-23 09:37:03 -0500

    Tweaks.

--- trickapi/eswrapper/managers.py
+++ trickapi/eswrapper/managers.py
@@ -1,3 +1,6 @@
+from .script import ES
+
+
 class ESManager(object):
 
     '''
@@ -12,12 +15,16 @@ class ESManager(object):
     def get_model(self):
         return self.model
 
+    def return_list(self, es_queryset):
+        return [doc['_source'] for doc in es_queryset['hits']['hits']]
+
     def all(self):
-        return ES.search(
+        es_qs = ES.search(
             index=[self.get_model().get_index_name()],
             doc_type=[self.get_model().get_document_type()],
             q='*',
-            size=self.get_model().objects.count())['hits']['hits']
+            size=self.get_model().objects.count())
+        return self.return_list(es_qs)
 
     # TODO: Finish this function
     def search(self, body):
